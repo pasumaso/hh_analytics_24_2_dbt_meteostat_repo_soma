e2f335c358f253cd7ca0489f84e170d41fbf62c2
Soma Pasumarthy <soma.pasumarthy@outlook.com>
2024-10-15 10:19:26 +0200

    e4

--- models/prep/prep_airports.sql
+++ models/prep/prep_airports.sql
@@ -1,7 +1,15 @@
 WITH airports_reorder AS (
-    SELECT faa
-    	   ,...
-    	   ...
+    SELECT  
+    faa, 
+    name, 
+    country,
+    region,
+    lat,
+    lon,
+    alt,
+    tz,
+    dst,
+    city    
     FROM {{ref('staging_airports')}}
 )
 SELECT * FROM airports_reorder
--- models/staging/staging_flights_one_month.sql
+++ models/staging/staging_flights_one_month.sql
@@ -1,6 +1,8 @@
+{{ config(materialized='view') }}
+
 WITH flights_one_month AS (
     SELECT * 
-    FROM 'staging_flights', 'flights'
+    FROM {{source('staging_flights', 'flights')}}
     WHERE DATE_PART('month', flight_date) = 1 
 )
 SELECT * FROM flights_one_month
